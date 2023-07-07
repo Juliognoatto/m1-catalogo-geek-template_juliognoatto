62f2bbc812f373950e24886fe0c856071d6da4b6
julio <juliognoatto5@gmail.com>
2023-07-07 15:45:56 -0300

    funções criadas, falta estilização

--- script.js
+++ script.js
@@ -14,4 +14,68 @@ const productsArray = [
     type: "Action Figures",
   },
   // Produto 3 (adicione os produtos restantes abaixo)
+
+  {
+    image: "./assets/img/paintings/quadro-controle.jpg",
+    name: "controle Playstation",
+    price: "R$99,00",
+    type: "Paintings",
+  },
+
+  // produto 4 
+  {
+    image: "./assets/img/paintings/quadro-mario.jpg",
+    name: "Mario",
+    price: "R$275,00",
+    type: "Paintings",
+  },
+  // produto 5 
+  {
+    image: "./assets/img/paintings/quadro-pacman.jpg",
+    name: "PacMan",
+    price: "R$160,00",
+    type: "Paintings",
+  },
+  // pruduto 6
+  {
+    image: "./assets/img/paintings/quadro-relogio.jpg",
+    name: "Smart Watch",
+    price: "R$120,00",
+    type: "Paintings",
+  },
+  // produto 7
+  {
+    image: "./assets/img/actions/godzilla-action-figure.jpg",
+    name: "godzilla",
+    price: "R$700,00",
+    type: "Action Figures",
+  },
+  // produto 8
+  {
+    image: "./assets/img/actions/groot-action-figure.jpg",
+    name: "Groot",
+    price: "R$950,00",
+    type: "Action Figures",
+  },
+  // produto 9
+  {
+    image: "./assets/img/actions/ironman-action-figure.jpg",
+    name: "Homem de Ferro",
+    price: "R$1030,00",
+    type: "Action Figures",
+  },
+  // produto 10
+  {
+    image: "./assets/img/actions/starwars-action-figure.jpg",
+    name: "Sr yoda",
+    price: "R$870,00",
+    type: "Action Figures",
+  },
+  // produto 11
+  {
+    image: "./assets/img/actions/wolverine-action-figure.jpg",
+    name: "Wolverine",
+    price: "R$510,00",
+    type: "Action Figures",
+  },
 ];
